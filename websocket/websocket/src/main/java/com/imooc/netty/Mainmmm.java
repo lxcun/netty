1907006e98ecaeb718816ce8ea4156a75c1e3060
lxcun <39074178+lxcun@users.noreply.github.com>
2019-04-19 16:41:34 +0800

    Add files via upload
    netty设计

---
+++ websocket/websocket/src/main/java/com/imooc/netty/Mainmmm.java
@@ -0,0 +1,8 @@
+package com.imooc.netty;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.Collection;
+
+public interface Mainmmm extends Collection,Cloneable {
+}
